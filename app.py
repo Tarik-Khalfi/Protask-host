45df44cd36b6482cba036c7f02fb2818a6979512
Tarik-Khalfi <87537254+Tarik-Khalfi@users.noreply.github.com>
2023-06-20 07:03:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from website import create_app as application
+from website import create_app 
 if __name__ == "__main__":
-    app = application
+    app = create_app()
     app.run(debug=False, host='0.0.0.0')
